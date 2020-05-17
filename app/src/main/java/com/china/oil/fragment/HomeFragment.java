837a9d309a70cb8c0326642844cc61d0f3a41b6b
Jiang dawei <dawei801230@163.com>
2020-05-17 17:11:33 +0800

    Change fragment

--- app/src/main/java/com/china/oil/fragment/HomeFragment.java
+++ app/src/main/java/com/china/oil/fragment/HomeFragment.java
@@ -12,14 +12,29 @@ import androidx.fragment.app.Fragment;
 
 import com.china.oil.R;
 import com.china.oil.uils.ScreenUtil;
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.ViewPortHandler;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -33,20 +48,38 @@ public class HomeFragment extends Fragment {
     @BindView(R.id.mLineChar)
     LineChart mLineChart;
 
+    @BindView(R.id.mTargetPieChart_1)
+    PieChart mTarPieChart1;
+    @BindView(R.id.mTargetPieChart_2)
+    PieChart mTarPieChart2;
+    @BindView(R.id.mTargetPieChart_3)
+    PieChart mTarPieChart3;
+
+    @BindView(R.id.mHorizontalBarChart)
+    HorizontalBarChart mHorizontalBarChart;
+    private final String lable[]={"手机","电视机","笔记本电脑","台式电脑", "电冰箱","空调","洗衣机","油烟机","空气净化器","加湿器"};
+    private float spaceForBar = 10f;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag_home, null);
         getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
         ButterKnife.bind(this, view);
 
-        initView();
+        initLineChart();
         //设置数据
         setData(12, 1000);
+
+        initTarPieChart();
+
+        initHorizontalBarChart();
         return view;
     }
 
-    private void initView(){
-
+    /**
+     * 折线图
+     * */
+    private void initLineChart(){
         // 线形图初始化
         ViewGroup.LayoutParams lp = mLineChart.getLayoutParams();
         lp.width = ScreenUtil.getWidth(getActivity());
@@ -106,6 +139,170 @@ public class HomeFragment extends Fragment {
         leftAxis.setGranularityEnabled(false);
 
         mLineChart.getAxisRight().setEnabled(false);
+    }
+
+    /**
+     * 环状图
+     * */
+    private void initTarPieChart(){
+        mTarPieChart1.setTouchEnabled(false);
+        mTarPieChart1.setDrawHoleEnabled(true); //显示中间的洞
+        mTarPieChart1.setHoleColor(0xffffffff); //洞的颜色
+        mTarPieChart1.setTransparentCircleColor(R.color.text_green);
+        mTarPieChart1.setDrawSliceText(false);//不显示切片里面的字体，就是pie 块里面的字体
+        mTarPieChart1.setDescription(null); //不显示描述
+        mTarPieChart1.setHoleRadius(70f); //洞的大小
+        mTarPieChart1.setTransparentCircleRadius(20f); //效果的大小
+        mTarPieChart1.setDrawCenterText(false);//中心的文字也不要写了
+        mTarPieChart1.setRotationAngle(108); //显示的角度 90+ X% * 360
+        Legend legend = mTarPieChart1.getLegend();
+        legend.setEnabled(false);//对pie 块的描述也不要显示
+
+        mTarPieChart1.setData(getPieData(70, 30, getResources().getColor(R.color.text_green)));
+        mTarPieChart1.highlightValues(null);
+        mTarPieChart1.animateY(1500, Easing.EaseInOutQuad);
+        mTarPieChart1.invalidate();
+        //环状图====================================================================================
+
+        mTarPieChart2.setTouchEnabled(false);
+        mTarPieChart2.setDrawHoleEnabled(true); //显示中间的洞
+        mTarPieChart2.setHoleColor(0xffffffff); //洞的颜色
+        mTarPieChart2.setTransparentCircleColor(R.color.sred);
+        mTarPieChart2.setDrawSliceText(false);//不显示切片里面的字体，就是pie 块里面的字体
+        mTarPieChart2.setDescription(null); //不显示描述
+        mTarPieChart2.setHoleRadius(70f); //洞的大小
+        mTarPieChart2.setTransparentCircleRadius(20f); //效果的大小
+        mTarPieChart2.setDrawCenterText(false);//中心的文字也不要写了
+        mTarPieChart2.setRotationAngle(108); //显示的角度 90+ X% * 360
+        Legend legend2 = mTarPieChart2.getLegend();
+        legend2.setEnabled(false);//对pie 块的描述也不要显示
+
+        mTarPieChart2.setData(getPieData(70, 30, getResources().getColor(R.color.sred)));
+        mTarPieChart2.highlightValues(null);
+        mTarPieChart2.animateY(1500, Easing.EaseInOutQuad);
+        mTarPieChart2.invalidate();
+
+        //环状图====================================================================================
+        mTarPieChart3.setTouchEnabled(false);
+        mTarPieChart3.setDrawHoleEnabled(true); //显示中间的洞
+        mTarPieChart3.setHoleColor(0xffffffff); //洞的颜色
+        mTarPieChart3.setTransparentCircleColor(R.color.layout_bg_blue);
+        mTarPieChart3.setDrawSliceText(false);//不显示切片里面的字体，就是pie 块里面的字体
+        mTarPieChart3.setDescription(null); //不显示描述
+        mTarPieChart3.setHoleRadius(70f); //洞的大小
+        mTarPieChart3.setTransparentCircleRadius(20f); //效果的大小
+        mTarPieChart3.setDrawCenterText(false);//中心的文字也不要写了
+        mTarPieChart3.setRotationAngle(108); //显示的角度 90+ X% * 360
+        Legend legend3 = mTarPieChart3.getLegend();
+        legend3.setEnabled(false);//对pie 块的描述也不要显示
+
+        mTarPieChart3.setData(getPieData(70, 30, getResources().getColor(R.color.layout_bg_blue)));
+        mTarPieChart3.highlightValues(null);
+        mTarPieChart3.animateY(1500, Easing.EaseInOutQuad);
+        mTarPieChart3.invalidate();
+
+    }
+
+    /**
+     * 水平条形图
+     * */
+    private void initHorizontalBarChart(){
+        // 水平条形图初始化
+        ViewGroup.LayoutParams lp = mHorizontalBarChart.getLayoutParams();
+        lp.width = ScreenUtil.getWidth(getActivity());
+        lp.height = ScreenUtil.getWidth(getActivity());
+        mHorizontalBarChart.setLayoutParams(lp);
+
+        //设置相关属性
+        mHorizontalBarChart.setTouchEnabled(false);
+        mHorizontalBarChart.setDrawBarShadow(false);
+        mHorizontalBarChart.setDrawValueAboveBar(true);
+        mHorizontalBarChart.getDescription().setEnabled(false);
+        mHorizontalBarChart.setPinchZoom(false);
+        mHorizontalBarChart.setNoDataText("无数据");
+        mHorizontalBarChart.setDrawGridBackground(false);
+
+        //x轴
+        XAxis xAxis = mHorizontalBarChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawAxisLine(false);
+        xAxis.setDrawGridLines(false);
+        xAxis.setAvoidFirstLastClipping(true);
+
+        xAxis.setDrawLabels(true);
+        xAxis.setGranularity(10f);
+
+        //y轴
+        YAxis yAxis = mHorizontalBarChart.getAxisLeft();
+        yAxis.setDrawAxisLine(false);
+        yAxis.setDrawGridLines(false);
+        yAxis.setAxisMinimum(0f);
+        yAxis.setEnabled(false);
+        //y轴
+        YAxis yr = mHorizontalBarChart.getAxisRight();
+        yr.setEnabled(false);
+
+        mHorizontalBarChart.setFitBars(true);
+        mHorizontalBarChart.animateY(2500);
+        Legend legend = mHorizontalBarChart.getLegend();
+        legend.setEnabled(false);
+
+        getHorizontalBarChartData();
+    }
+
+    /**
+     * 获取HorizontalBarChart数据
+     * */
+    private void getHorizontalBarChartData(){
+        XAxis xAxis = mHorizontalBarChart.getXAxis();
+        xAxis.setLabelCount(10);
+//        xAxis.setValueFormatter(new IAxisValueFormatter() {
+//            @Override
+//            public String getFormattedValue(float value, AxisBase axis) {
+//                return lable[(int) (value / spaceForBar)];
+//            }
+//
+//            @Override
+//            public int getDecimalDigits() {
+//                return 0;
+//            }
+//        });
+
+        float barWidth = 8f;
+        ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
+        for (int i = 0; i < 10; i++) {
+            float val = new Random().nextInt(1000);
+            yVals1.add(new BarEntry(i * spaceForBar, val));
+        }
+        BarDataSet set1;
+        if (mHorizontalBarChart.getData() != null &&
+                mHorizontalBarChart.getData().getDataSetCount() > 0) {
+            set1 = (BarDataSet) mHorizontalBarChart.getData().getDataSetByIndex(0);
+            set1.setValues(yVals1);
+            mHorizontalBarChart.getData().notifyDataChanged();
+            mHorizontalBarChart.notifyDataSetChanged();
+        } else {
+            set1 = new BarDataSet(yVals1, "DataSet 1");
+            set1.setColor(0xffff7700);
+            set1.setDrawValues(true);
+
+            //显示为整数
+//            set1.setValueFormatter(new IValueFormatter() {
+//
+//                @Override
+//                public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
+//                    int valuedate = (int) value;
+//                    return valuedate + "";
+//                }
+//            });
+            ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
+            dataSets.add(set1);
+
+            BarData data = new BarData(dataSets);
+            data.setValueTextSize(10f);
+            data.setBarWidth(barWidth);
+            mHorizontalBarChart.setData(data);
+        }
     }
 
     //设置数据
@@ -191,5 +388,45 @@ public class HomeFragment extends Fragment {
             //设置数据
             mLineChart.setData(data);
         }
+    }
+
+    /**
+     * 组合pie chart 的数据
+     *
+     * @param completeNum   完成的
+     * @param remainNum     剩下的
+     * @param completeColor 颜色
+     * @return
+     */
+    private PieData getPieData(int completeNum, int remainNum, int completeColor) {
+        //X轴数据
+        ArrayList<String> xValues = new ArrayList<String>();
+        xValues.add("1");
+        xValues.add("2");
+        xValues.add("3");
+
+        //Y轴数据 -- start ************************************* //
+        ArrayList<PieEntry> yValues = new ArrayList<PieEntry>();
+        yValues.add(new PieEntry(completeNum, 0));
+        yValues.add(new PieEntry(remainNum, 1));
+        yValues.add(new PieEntry((completeNum + remainNum) / 9f, 2));
+        PieDataSet dataSet = new PieDataSet(yValues, "Election Results");
+        dataSet.setSliceSpace(0f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setDrawValues(false);
+        //Y轴数据 -- end ************************************* //
+
+        //颜色值
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+
+        colors.add(completeColor);
+        colors.add(0xffe9e9e9);
+        colors.add(0xffffffff);
+        dataSet.setColors(colors);
+
+        //设置数据开始画
+        PieData data = new PieData(dataSet);
+        data.setValueFormatter(new PercentFormatter());
+        return data;
     }
 }
