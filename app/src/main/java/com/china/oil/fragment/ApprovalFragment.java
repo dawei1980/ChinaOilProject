4ede4f58183e93816af20905ce41bda950c35a75
Jiang dawei <dawei801230@163.com>
2020-05-26 10:48:00 +0800

    add mine

--- app/src/main/java/com/china/oil/fragment/ApprovalFragment.java
+++ app/src/main/java/com/china/oil/fragment/ApprovalFragment.java
@@ -1,5 +1,6 @@
 package com.china.oil.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,11 +10,14 @@ import android.view.WindowManager;
 import androidx.fragment.app.Fragment;
 
 import com.china.oil.R;
+import com.china.oil.activity.LoginActivity;
 
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ApprovalFragment extends Fragment {
     private View view;
+    private Intent intent;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag_approval, null);
@@ -27,5 +31,40 @@ public class ApprovalFragment extends Fragment {
 
     private void initView(){
 
+    }
+
+    @OnClick({R.id.approval_layout,
+            R.id.under_approval_layout,
+            R.id.approval_result_layout,
+            R.id.search_contact_layout,
+            R.id.personal_center_layout,
+            R.id.sign_layout})
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.approval_layout:
+                intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.under_approval_layout:
+                intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.approval_result_layout:
+                intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.search_contact_layout:
+                intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.sign_layout:
+                intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.personal_center_layout:
+                intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
